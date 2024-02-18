a18f249b67f639e1fef163944f90bee755bb225b
jesusik <susannaablaeva3@gmail.com>
2024-02-18 14:16:08 +0500

    working post

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -5,17 +5,18 @@ const Camera = () => {
   const videoRef = useRef(null);
   const photoRef = useRef(null);
   const [isFrontCamera, setIsFrontCamera] = useState(false);
-
+  const [permissionGranted, setPermissionGranted] = useState(null);
+  const [responseData, setResponseData] = useState(null); // State to store fetched data
+  const url = "http://172.24.200.54:8000/api/v1/image_process/";
   useEffect(() => {
-    const constraints = {
-      video: {
-        facingMode: isFrontCamera ? "user" : "environment", // 'user' for front camera, 'environment' for back camera
-      },
-    };
     const initializeCamera = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
-          video: { width: 1080, height: 1920 }, constraints
+          video: {
+            facingMode: isFrontCamera ? "user" : "environment",
+            height: 1080,
+            width: 1920,
+          },
         });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
@@ -25,30 +26,45 @@ const Camera = () => {
             });
           });
         }
+        setPermissionGranted(true); // Set permission as granted after successful camera initialization
       } catch (error) {
         console.error("Error accessing camera:", error);
+        setPermissionGranted(false);
       }
     };
 
     initializeCamera();
-
-    return () => {
-      const currentVideoRef = videoRef.current;
-      if (currentVideoRef && currentVideoRef.srcObject) {
-        const stream = currentVideoRef.srcObject;
-        const tracks = stream.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
-    };
   }, [isFrontCamera]);
-
+//getting info
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://api.example.com/data");
+      setResponseData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+//switch
   const handleCameraSwitch = () => {
     setIsFrontCamera((prevState) => !prevState);
+    console.log(isFrontCamera);
   };
+
+  const postData = async () => {
+    axios.get(url)
+    .then(response => {
+      setResponseData(response.data.message);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  };
+
+
 
   const takePhoto = async () => {
     const width = 414;
-    const height = width / (9 / 16);
+    const height = 800;
     const video = videoRef.current;
     const photo = photoRef.current;
     photo.width = width;
@@ -56,27 +72,40 @@ const Camera = () => {
 
     const ctx = photo.getContext("2d");
     ctx.drawImage(video, 0, 0, width, height);
-
     // Convert canvas image to base64 data URL
     const dataUrl = photo.toDataURL("image/jpeg");
 
-    // Send dataUrl to server using Axios
     try {
-      const response = await axios.post("https://httpbin.org/post", {
+      const response = await axios.post(url, {
         image: dataUrl,
       });
       console.log("Image uploaded successfully:", response.data);
-      // Handle success, e.g., show a success message
+      // fetchData(); // Fetch data after successfully posting the image
     } catch (error) {
       console.error("Error uploading image:", error);
-      // Handle error, e.g., show an error message
     }
   };
 
+  // if no camera access
+  if (permissionGranted === false) {
+    return (
+      <div>
+        Camera permission denied. Please grant access to the camera in your
+        browser settings.
+      </div>
+    );
+  }
+
   return (
     <>
+      {responseData && (
+        <div className="responseData">
+          <b>5000</b>
+        </div>
+      )}
+
       <div className="camera">
-        <video onClick={takePhoto} ref={videoRef} autoPlay></video>
+        <video onClick={takePhoto} ref={videoRef}></video>
         <button onClick={handleCameraSwitch}>switch</button>
       </div>
       <canvas ref={photoRef} style={{ display: "none" }}></canvas>
